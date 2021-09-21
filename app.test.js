988da15b3c63ddddbc18f143a4b0899ad0a43f19
Enrique Vergara <EnriqueVergara@CLME2107-0030.local>
2021-09-21 17:42:25 -0500

    test add app.test.js to exec test library

---
+++ app.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+app.get('/user', function(req, res) {
+  res.status(200).json({ name: 'john' });
+});
+
+describe('GET /user', function() {
+  it('responds with json', function(done) {
+    request(app)
+      .get('/user')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
